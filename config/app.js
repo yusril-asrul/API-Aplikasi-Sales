5b406d1601c99b1c9a3880e7057f656405464476
Yusril Asrul <yusrilasrul122@gmail.com>
2022-12-28 11:03:10 +0800

    update port

--- config/app.js
+++ config/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-    port : 4444,
+    port : 5020,
     jwtSecret : process.env.JWT_SECRET,
     urlUpload : process.env.URL_UPLOAD_IMAGE,
     dirUpload : process.env.DIR_UPLOAD_IMAGE
