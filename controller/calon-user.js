81230729666ce6ed8bbeef306f0b08bf3f966d59
muflih95 <ahmadmuflih1@gmail.com>
2023-01-11 13:36:41 +0800

    perbaikan data calon user

--- controller/calon-user.js
+++ controller/calon-user.js
@@ -4,8 +4,12 @@ const {responseError,responseSuccess} = require('../utils/response');
 
 const load = async (req,res) => {
     let id = req.params.id ? req.params.id : '';
+    let cari = req.params.cari ? req.params.cari : '';
+    let first_date = req.params.first_date ? req.params.first_date : '';
+    let last_date = req.params.last_date ? req.params.last_date : '';
+
     const idUser = req.auth.user.id
-    let data = await loadDataCalonUser(idUser,id);
+    let data = await loadDataCalonUser(idUser,id,cari,first_date,last_date);
     if (data){
         return responseSuccess(res,'Loaded.',data)
     } else {
@@ -26,7 +30,7 @@ const hapus = async (req,res) => {
 
 const save = async function(req,res){
     try {
-        const {nama_usaha,pemilik_usaha,nohp,alamat,lat,long,foto} = req.body
+        const {nama_usaha,pemilik_usaha,nohp,alamat,lat,long,foto,jenis_usaha,jenis_user} = req.body
         const {applikasi_yang_digunakan,lama_tahun,lama_bulan,masa_berakhir} = req.body
         const idUser = req.auth.user.id
         const jns = req.jenis
@@ -35,7 +39,7 @@ const save = async function(req,res){
         let message = ''
         let data = {
             id_user : idUser,
-            nama_usaha,pemilik_usaha,nohp,alamat,
+            nama_usaha,pemilik_usaha,nohp,alamat,jenis_usaha,jenis_user,
             map_lat : lat,
             map_long : long,
             applikasi_yang_digunakan,
@@ -66,11 +70,11 @@ const save = async function(req,res){
 
 const update_status = async function(req,res){
     try {
-        const {id,status} = req.body
+        const {id,status,keterangan} = req.body
 
         let message = 'Berhasil Update Status'
         let data = {
-            status : status
+            status,keterangan
         }
 
         let jns = "EDIT";
--- models/calon-user.js
+++ models/calon-user.js
@@ -11,23 +11,37 @@ module.exports = {
     async hapusDataCalonUser(id){
         return await modelHelper.deleteById(connection,table,id)
     },
-    async loadDataCalonUser(idUser,id=''){
+    async loadDataCalonUser(idUser,id='',cari='',first_date='',last_date=''){
         let data = false;
         let detail = false 
 
         let query = `
-            SELECT id,nama_usaha,pemilik_usaha,nohp,alamat,map_lat, map_long,
+            SELECT id,nama_usaha,pemilik_usaha,nohp,alamat,
+            jenis_usaha,
+            jenis_user,
+            map_lat, map_long,
             applikasi_yang_digunakan,lama_tahun,lama_bulan,
             masa_berakhir,
-            CONCAT('${dirUpload}',foto) as url_image,status,
+            CONCAT('${dirUpload}',foto) as url_image,
+            status,
+            keterangan,
             DATE_FORMAT(createdAt, "%d/%m/%Y %H:%i:%s") as create_at
             FROM ${table} WHERE id_user = '${idUser}'`
         if (id) {
-            query += ` AND id='${id}' ORDER BY id DESC`
+            query += ` AND id='${id}'`
             detail = true
-        }else{
-            query += ` ORDER BY id DESC`
         }
+        if(cari) {
+            query += ` AND (
+                nama_usaha like '%${cari}%'
+                or pemilik_usaha like '%${cari}%'
+            )`
+        }
+        if(first_date) {
+            query += ` AND date(createdAt) between '${first_date}' and '${last_date}'`
+        }
+
+        query += ` ORDER BY id DESC`
 
         let rows = await modelHelper.getRowsQuery(connection,query)
 
