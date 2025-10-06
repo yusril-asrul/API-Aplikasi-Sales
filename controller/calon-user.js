150b2e1c4a783ce1cc07d1c3ffcd77405a0b6710
muflih95 <ahmadmuflih1@gmail.com>
2025-10-06 16:37:34 +0800

    tambah validasi no hp, ketika input data user, dan buat log update status

--- controller/calon-user.js
+++ controller/calon-user.js
@@ -1,98 +1,116 @@
-const { simpanDataCalonUser, hapusDataCalonUser, loadDataCalonUser } = require('../models');
+const { simpanDataCalonUser, hapusDataCalonUser, loadDataCalonUser, cekNoHp, simpanLogCalonUser } = require('../models');
 const { randomString, uploadBase64toPhpCI3 } = require('../utils/helper');
-const {responseError,responseSuccess} = require('../utils/response');
+const { responseError, responseSuccess } = require('../utils/response');
 
-const load = async function(req,res){
+const load = async function (req, res) {
     try {
         let id = req.params.id ? req.params.id : '';
-        const {cari,first_date,last_date} = req.body
+        const { cari, first_date, last_date } = req.body
 
         const idUser = req.auth.user.id
-        let data = await loadDataCalonUser(idUser,id,cari,first_date,last_date);
-        if (data){
-            return responseSuccess(res,'Loaded.',data)
+        let data = await loadDataCalonUser(idUser, id, cari, first_date, last_date);
+        if (data) {
+            return responseSuccess(res, 'Loaded.', data)
         } else {
-            return responseSuccess(res,'data not found.',{},false)
+            return responseSuccess(res, 'data not found.', {}, false)
         }
-    } catch(error){
+    } catch (error) {
         console.log(error)
-        return responseError(res,error,500);
+        return responseError(res, error, 500);
     }
 }
 
-const hapus = async (req,res) => {
+const hapus = async (req, res) => {
     try {
         let id = req.params.id
         await hapusDataCalonUser(id)
-        return responseSuccess(res,'Deleted.')
-    } catch(error){
+        return responseSuccess(res, 'Deleted.')
+    } catch (error) {
         console.log(error)
-        return responseError(res,error,500);
+        return responseError(res, error, 500);
     }
 }
 
-const save = async function(req,res){
+const save = async function (req, res) {
     try {
-        const {nama_usaha,pemilik_usaha,nohp,alamat,lat,long,foto,jenis_usaha,jenis_user} = req.body
-        const {applikasi_yang_digunakan,lama_tahun,lama_bulan,masa_berakhir,catatan} = req.body
+        const { nama_usaha, pemilik_usaha, nohp, alamat, lat, long, foto, jenis_usaha, jenis_user } = req.body
+        const { applikasi_yang_digunakan, lama_tahun, lama_bulan, masa_berakhir, catatan } = req.body
         const idUser = req.auth.user.id
         const jns = req.jenis
 
+        if (nohp == '') {
+            return responseSuccess(res, 'Nomor HP Tidak Boleh Kosong.')
+        }
+
+        let validateHp = await cekNoHp(nohp);
+        if (!validateHp) {
+            return responseSuccess(res, 'Nomor HP Sudah Terdaftar.')
+        }
 
         let message = ''
         let data = {
-            id_user : idUser,
-            nama_usaha,pemilik_usaha,nohp,alamat,jenis_usaha,jenis_user,
-            map_lat : lat,
-            map_long : long,
+            id_user: idUser,
+            nama_usaha, pemilik_usaha, nohp, alamat, jenis_usaha, jenis_user,
+            map_lat: lat,
+            map_long: long,
             applikasi_yang_digunakan,
-            lama_tahun,lama_bulan,masa_berakhir,
+            lama_tahun, lama_bulan, masa_berakhir,
             catatan
         }
 
         if (foto) {
-            let filename = randomString(10)+'.jpg';
-            await uploadBase64toPhpCI3(filename,foto)
+            let filename = randomString(10) + '.jpg';
+            await uploadBase64toPhpCI3(filename, foto)
             data.foto = filename
         }
 
-        if (jns === "ADD"){
-            await simpanDataCalonUser(jns,data)
+        if (jns === "ADD") {
+            await simpanDataCalonUser(jns, data)
             message = 'Created.'
         } else if (jns === "EDIT") {
             let id = req.params.id
-            await simpanDataCalonUser(jns,data,id,'id')
+            await simpanDataCalonUser(jns, data, id, 'id')
             message = 'Edited.'
         }
 
-        return responseSuccess(res,message)
-    } catch(error){
+        return responseSuccess(res, message)
+    } catch (error) {
         console.log(error)
-        return responseError(res,error,500);
+        return responseError(res, error, 500);
     }
 }
 
-const update_status = async function(req,res){
+const update_status = async function (req, res) {
     try {
-        const {id,status,keterangan,nominal} = req.body
+        const { id, status, keterangan, nominal } = req.body
+        const idUser = req.auth.user.id
 
         let message = 'Berhasil Update Status'
         let data = {
-            status,keterangan
+            status, keterangan
         }
 
-        if (status == "Closing"){
+        if (status == "Closing") {
             data.nominal = nominal
         }
 
         let jns = "EDIT";
-        await simpanDataCalonUser(jns,data,id,'id')
+        await simpanDataCalonUser(jns, data, id, 'id')
 
-        return responseSuccess(res,message)
-    } catch(error){
+
+        let dataLog = {
+            id_calon_user: id,
+            status,
+            keterangan,
+            create_by: idUser
+        }
+        await simpanLogCalonUser('ADD', dataLog, id, 'id')
+
+        return responseSuccess(res, message)
+    } catch (error) {
         console.log(error)
-        return responseError(res,error,500);
+        return responseError(res, error, 500);
     }
 }
 
-module.exports = {save,hapus,load,update_status}
+module.exports = { save, hapus, load, update_status }
--- models/calon-user.js
+++ models/calon-user.js
@@ -1,19 +1,24 @@
 const connection = require('../config/database');
 const modelHelper = require('./model-helper');
-const {dirUpload} = require('../config/app')
+const { dirUpload } = require('../config/app')
 const table = 'calon_user'
+const tableLog = 'log_calon_user'
 
 module.exports = {
-    async simpanDataCalonUser(jns,data,id='',key=''){
-        let query = await modelHelper.saveRowQuery(connection,table,data,id,jns,key)
+    async simpanDataCalonUser(jns, data, id = '', key = '') {
+        let query = await modelHelper.saveRowQuery(connection, table, data, id, jns, key)
         return query
     },
-    async hapusDataCalonUser(id){
-        return await modelHelper.deleteById(connection,table,id)
+    async simpanLogCalonUser(jns, data, id = '', key = '') {
+        let query = await modelHelper.saveRowQuery(connection, tableLog, data, id, jns, key)
+        return query
     },
-    async loadDataCalonUser(idUser,id='',cari='',first_date='',last_date=''){
+    async hapusDataCalonUser(id) {
+        return await modelHelper.deleteById(connection, table, id)
+    },
+    async loadDataCalonUser(idUser, id = '', cari = '', first_date = '', last_date = '') {
         let data = false;
-        let detail = false 
+        let detail = false
 
         let query = `
             SELECT id,nama_usaha,pemilik_usaha,nohp,alamat,
@@ -32,26 +37,43 @@ module.exports = {
             query += ` AND id='${id}'`
             detail = true
         }
-        if(cari) {
+        if (cari) {
             query += ` AND (
                 nama_usaha like '%${cari}%'
                 or pemilik_usaha like '%${cari}%'
             )`
         }
-        if(first_date) {
+        if (first_date) {
             query += ` AND date(createdAt) between '${first_date}' and '${last_date}'`
         }
 
         query += ` ORDER BY id DESC`
 
-        let rows = await modelHelper.getRowsQuery(connection,query)
+        let rows = await modelHelper.getRowsQuery(connection, query)
 
-        if (detail){
-            if (rows.length > 0){
+        if (detail) {
+            if (rows.length > 0) {
                 data = rows[0]
             }
         } else {
             data = rows
+        }
+
+        return data
+    },
+    async cekNoHp(nohp) {
+        let data = true;
+
+        let query = `
+            SELECT id
+            FROM ${table} WHERE nohp = '${nohp}'`
+
+        let rows = await modelHelper.getRowsQuery(connection, query)
+
+        if (rows) {
+            if (rows.length > 0) {
+                data = false
+            }
         }
 
         return data
